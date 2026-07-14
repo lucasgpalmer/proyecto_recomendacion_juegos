b67a3e9f7f6f225565455e61e412e0e734c9f06e
lucasgpalmer <lucasgonzalezpalmer@gmail.com>
2026-07-13 23:54:57 -0300

    Completado la funcion para pedir datos al usuario

--- PROYECTO/nuevo main/usuarios.py
+++ PROYECTO/nuevo main/usuarios.py
@@ -2,10 +2,13 @@ from inputs import *
 from menu import *
 
 usuarios = [] # ver donde poner, ahora tmb esta en main
-generos = ["masculino", "femenino", "no binario"] #ver donde meterlos
+generos = ["Masculino", "Femenino", "No Binario"] #ver donde meterlos
 generos_juegos = ["Accion", "Aventura", "RPG", "Estrategia", "Simulacion", "Deportes", "Carreras", "Terror", "Puzzle", "Plataformas", "Sandbox", "Roguelike", "Supervivencia", "Lucha"]
 modos_de_juego = ["Solo", "Cooperativo", "Competitivo"]
-
+niveles_compromiso = ["Casual", "Dedicado", "Competitivo"]
+duraciones_preferidas = ["Corto", "Medio", "Largo"]
+plataformas = ["PC", "PlayStation", "Xbox", "Switch"]
+gamas = ["Baja", "Media", "Alta"]
 
 def ingresar_usuario(usuarios : list):
     usuario = {}
@@ -20,12 +23,12 @@ def ingresar_usuario(usuarios : list):
     while not validar_rango(usuario["edad"], 8, 100):
         usuario["edad"] = pedir_entero("ERROR. Ingrese una edad validad (Minímo 8 años, máximo 100): ", "ERROR. Ingrese una edad válida solo usando números (Minímo 8 años, máximo 100): ")
 
-    usuario["genero"] = pedir_cadena("Ingrese su genero [Masculino / Femenino / No Binario]: ", "ERROR. Ingrese un genero valido [Masculino / Femenino / No Binario]: ", True)
+    usuario["genero"] = pedir_cadena("Ingrese su genero [Masculino/Femenino/No Binario]: ", "ERROR. Ingrese un genero valido [Masculino/Femenino/No Binario]: ", True)
     while not validar_coincidencia(usuario["genero"], generos):
-        usuario["genero"] = pedir_cadena("ERROR. Ingrese un genero valido [Masculino / Femenino / No Binario]: " ,"ERROR. Ingrese un genero valido [Masculino / Femenino / No Binario]: ", True)
+        usuario["genero"] = pedir_cadena("ERROR. Ingrese un genero valido [Masculino/Femenino/No Binario]: " ,"ERROR. Ingrese un genero valido [Masculino/Femenino/No Binario]: ", True)
 
     mostrar_menu_genero_juegos(generos_juegos) # FALTA LOGICA PARA QUE SI ALCANZA EL TOTAL DE OPCIONES QUE BREAKEE AUTOMATICAMENTE. LO DEJO PENDIENTE
-    usuario["generos_juego"] = []
+    usuario["generos_juego"] = []   
     primera = True
     while True:
         ya_existe = False
@@ -74,21 +77,189 @@ def ingresar_usuario(usuarios : list):
                     print(f"{modo_de_juego} ya fue seleccionado!")
                     ya_existe = True
                     break
+
         if not ya_existe:
             usuario["modos_de_juego"].append(modo_de_juego)
 
-    usuario["nivel_de_compromiso"]
-    usuario["duracion_preferida"]
-    usuario["plataforma"]
-    usuario["gama"]
-    usuario["espacio_disco"]
-    usuario["presupuesto"]
+    usuario["niveles_compromiso"] = [] # FALTA LOGICA PARA QUE SI ALCANZA EL TOTAL DE OPCIONES QUE BREAKEE AUTOMATICAMENTE. LO DEJO PENDIENTE
+    primera = True
+    while True:
+        ya_existe = False
+        if primera:
+            nivel_compromiso = pedir_cadena(
+                "Ingrese un nivel de compromiso [Casual/Dedicado/Competitivo]: ",
+                "ERROR. Ingrese un nivel de compromiso valido [Casual/Dedicado/Competitivo]: ",
+                True
+            )
+            while not validar_coincidencia(nivel_compromiso, niveles_compromiso):
+                nivel_compromiso = pedir_cadena(
+                    "ERROR. Ingrese un nivel de compromiso valido [Casual/Dedicado/Competitivo]: ",
+                    "ERROR. Ingrese un nivel de compromiso valido [Casual/Dedicado/Competitivo]: ",
+                    True
+                )
+            primera = False
+        else:
+            print("Niveles de compromiso seleccionados: ", end="")
+            mostrar_lista(usuario["niveles_compromiso"])
+
+            nivel_compromiso = pedir_cadena(
+                "Ingrese otro nivel de compromiso [Casual/Dedicado/Competitivo] o [N] para finalizar: ",
+                "ERROR. Ingrese un nivel de compromiso valido [Casual/Dedicado/Competitivo] o [N]: ",
+                True
+            )
+            while not validar_coincidencia(nivel_compromiso, niveles_compromiso) and nivel_compromiso != "n":
+                nivel_compromiso = pedir_cadena(
+                    "ERROR. Ingrese un nivel de compromiso valido [Casual/Dedicado/Competitivo] o [N]: ",
+                    "ERROR. Ingrese un nivel de compromiso valido [Casual/Dedicado/Competitivo] o [N]: ",
+                    True
+                )
+
+            if nivel_compromiso == "n":
+                break
+
+            for nivel in usuario["niveles_compromiso"]:
+                if nivel == nivel_compromiso:
+                    print(f"{nivel_compromiso} ya fue seleccionado!")
+                    ya_existe = True
+                    break
+
+        if not ya_existe:
+            usuario["niveles_compromiso"].append(nivel_compromiso)
+
+
+    usuario["duraciones_preferidas"] = [] # FALTA LOGICA PARA QUE SI ALCANZA EL TOTAL DE OPCIONES QUE BREAKEE AUTOMATICAMENTE. LO DEJO PENDIENTE
+    primera = True
+    while True:
+        ya_existe = False
+        if primera:
+            duracion_preferida = pedir_cadena(
+                "Ingrese una duración preferida [Corto/Medio/Largo]: ",
+                "ERROR. Ingrese una duración preferida valida [Corto/Medio/Largo]: ",
+                True
+            )
+            while not validar_coincidencia(duracion_preferida, duraciones_preferidas):
+                duracion_preferida = pedir_cadena(
+                    "ERROR. Ingrese una duración preferida valida [Corto/Medio/Largo]: ",
+                    "ERROR. Ingrese una duración preferida valida [Corto/Medio/Largo]: ",
+                    True
+                )
+            primera = False
+        else:
+            print("Duraciones preferidas seleccionadas: ", end="")
+            mostrar_lista(usuario["duraciones_preferidas"])
+
+            duracion_preferida = pedir_cadena(
+                "Ingrese otra duración preferida [Corto/Medio/Largo] o [N] para finalizar: ",
+                "ERROR. Ingrese una duración preferida valida [Corto/Medio/Largo] o [N]: ",
+                True
+            )
+
+            while not validar_coincidencia(duracion_preferida, duraciones_preferidas) and duracion_preferida != "n":
+                duracion_preferida = pedir_cadena(
+                    "ERROR. Ingrese una duración preferida valida [Corto/Medio/Largo] o [N]: ",
+                    "ERROR. Ingrese una duración preferida valida [Corto/Medio/Largo] o [N]: ",
+                    True
+                )
+
+            if duracion_preferida == "n":
+                break
+
+            for duracion in usuario["duraciones_preferidas"]:
+                if duracion == duracion_preferida:
+                    print(f"{duracion_preferida} ya fue seleccionado!")
+                    ya_existe = True
+                    break
+
+        if not ya_existe:
+            usuario["duraciones_preferidas"].append(duracion_preferida)
+
+    usuario["plataformas"] = [] # FALTA LOGICA PARA QUE SI ALCANZA EL TOTAL DE OPCIONES QUE BREAKEE AUTOMATICAMENTE. LO DEJO PENDIENTE
+    primera = True
+    while True:
+        ya_existe = False
+        if primera:
+            plataforma = pedir_cadena(
+                "Ingrese una plataforma [PC/PlayStation/Xbox/Switch]: ",
+                "ERROR. Ingrese una plataforma valida [PC/PlayStation/Xbox/Switch]: ",
+                True
+            )
+            while not validar_coincidencia(plataforma, plataformas):
+                plataforma = pedir_cadena(
+                    "ERROR. Ingrese una plataforma valida [PC/PlayStation/Xbox/Switch]: ",
+                    "ERROR. Ingrese una plataforma valida [PC/PlayStation/Xbox/Switch]: ",
+                    True
+                )
+            primera = False
+        else:
+            print("Plataformas seleccionadas: ", end="")
+            mostrar_lista(usuario["plataformas"])
+
+            plataforma = pedir_cadena(
+                "Ingrese otra plataforma [PC/PlayStation/Xbox/Switch] o [N] para finalizar: ",
+                "ERROR. Ingrese una plataforma valida [PC/PlayStation/Xbox/Switch] o [N]: ",
+                True
+            )
+
+            while not validar_coincidencia(plataforma, plataformas) and plataforma != "n":
+                plataforma = pedir_cadena(
+                    "ERROR. Ingrese una plataforma valida [PC/PlayStation/Xbox/Switch] o [N]: ",
+                    "ERROR. Ingrese una plataforma valida [PC/PlayStation/Xbox/Switch] o [N]: ",
+                    True
+                )
+
+            if plataforma == "n":
+                break
+
+            for plataforma_seleccionada in usuario["plataformas"]:
+                if plataforma_seleccionada == plataforma:
+                    print(f"{plataforma} ya fue seleccionada!")
+                    ya_existe = True
+                    break
+
+        if not ya_existe:
+            usuario["plataformas"].append(plataforma)
+
+    usuario["gama"] = "-"
+    for plataforma in usuario["plataformas"]:
+        if plataforma == "pc":
+            usuario["gama"] = pedir_cadena(
+                "Ingrese la gama de su PC [Baja / Media / Alta]: ",
+                "ERROR. Ingrese una gama valida [Baja / Media / Alta]: ",
+                True
+            )
+            while not validar_coincidencia(usuario["gama"], gamas):
+                usuario["gama"] = pedir_cadena(
+                    "ERROR. Ingrese una gama valida [Baja / Media / Alta]: ",
+                    "ERROR. Ingrese una gama valida [Baja / Media / Alta]: ",
+                    True
+                )
+            break
+
+    usuario["espacio_disco"] = pedir_entero(
+        "Ingrese su espacio en disco disponible (GB): ",
+        "ERROR. Ingrese solo un valor numerico: "
+    )
+    while usuario["espacio_disco"] < 1:
+        usuario["espacio_disco"] = pedir_entero(
+            "ERROR. Ingrese un espacio mayor a 0 GB: ",
+            "ERROR. Ingrese solo un valor numerico: "
+        )
+
+    usuario["presupuesto"] = pedir_entero(
+        "Ingrese su presupuesto disponible (USD): ",
+        "ERROR. Ingrese un valor numerico no negativo: "
+    )
 
     usuarios.append(usuario)
+    # retornar usuario? desp veo beneficios
 
 def mostrar_usuario(usuario : dict):
     for key in usuario:
-        print(f"{key}: {usuario[key]}")
+        if type(usuario[key]) == list:
+            print(f"{key}: ", end="")
+            mostrar_lista(usuario[key])
+        else:   
+            print(f"{key}: {usuario[key]}")
 
 
 
